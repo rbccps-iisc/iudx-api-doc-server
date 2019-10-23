c80a39024304e45268229f3e9a1ec75a431ce746
rnaaz <rohinanaaz@gmail.com>
2019-10-23 13:55:32 +0530

    Added the api-examples codes for search & count both for all the attributes | updated the rs.yaml file

---
+++ api-examples/rs/search/Go/attribute_PropertyIsGreaterThanOrEqualTo.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+
+	url := "https://pune.iudx.org.in/resource-server/pscdcl/v1/search"
+
+	payload := strings.NewReader("{\n\t\"id\" : \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/pscdcl/aqm-bosch-climo/Ambedkar society circle_29\",\n\t\"attribute-name\" : \"LIGHT\",\n\t\"attribute-value\": \"2900\",\n\t\"comparison-operator\": \"propertyisgreaterthanequalto\",\n\t\"options\":\"latest\"\n}")
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("Content-Type", "application/json")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+}
