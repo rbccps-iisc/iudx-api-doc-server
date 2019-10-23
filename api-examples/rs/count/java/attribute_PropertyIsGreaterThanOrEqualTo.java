c6b35bf5ac7318add3f1e58ff7dffd62984487e3
Rnaaz <rohinanaaz@gmail.com>
2019-10-23 14:13:32 +0530

    Added the api-examples codes for search & count both for all the attributes | updated the rs.yaml file (#11)

---
+++ api-examples/rs/count/java/attribute_PropertyIsGreaterThanOrEqualTo.java
@@ -0,0 +1,11 @@
+OkHttpClient client = new OkHttpClient();
+
+MediaType mediaType = MediaType.parse("application/json");
+RequestBody body = RequestBody.create(mediaType, "{\n\t\"id\" : \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/pscdcl/aqm-bosch-climo/Ambedkar society circle_29\",\n\t\"attribute-name\" : \"LIGHT\",\n\t\"attribute-value\": \"2900\",\n\t\"comparison-operator\": \"propertyisgreaterthanequalto\",\n\t\"options\":\"latest\"\n}");
+Request request = new Request.Builder()
+  .url("https://pune.iudx.org.in/resource-server/pscdcl/v1/count")
+  .post(body)
+  .addHeader("Content-Type", "application/json")
+  .build();
+
+Response response = client.newCall(request).execute();
---
+++ api-examples/rs/count/js/attribute_PropertyIsNotEqualTo.js
@@ -0,0 +1,15 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://pune.iudx.org.in/resource-server/pscdcl/v1/count",
+  "method": "POST",
+  "headers": {
+    "Content-Type": "application/json",
+  },
+  "processData": false,
+  "data": "{\n\t\"id\" : \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/pscdcl/aqm-bosch-climo/Ambedkar society circle_29\",\n\t\"attribute-name\" : \"LIGHT\",\n\t\"attribute-value\": \"2900\",\n\t\"comparison-operator\": \"propertyisnotequalto\",\n\t\"options\":\"latest\"\n}"
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
