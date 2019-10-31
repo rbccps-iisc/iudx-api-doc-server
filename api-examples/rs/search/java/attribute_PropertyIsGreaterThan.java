451db429ec0c46742c315fa1ed778228f97124ca
Jishnu P <jishnu.jayakumar182@gmail.com>
2019-10-31 17:22:30 +0530

    Add RS API codes (#20)
    * REARRANGE yaml keys
    
    * ADD copy to clipboard feature for copying code on button click
    
    * FIX a bug: Copy code was not copying the code exactly (\n,\t etc. were not read in the exact form)
    
    * UPDATE port to 443
    
    * ADD Multithreading to nodejs event loop
    
    * DELETE auth.zip
    
    * ADD RS-Latest Data codes
    
    * ADD RS - Temporal During codes
    
    * ADD RS - Temporal During codes (/count)
    
    * ADD RS - Temporal before codes (search)
    
    * ADD RS - Temporal After codes (/search)
    
    * ADD RS - Temporal Equals codes (/search)
    
    * ADD RS - Spatial Circle codes (s)
    
    * ADD RS - response filter (/search, latest, temporal-before)
    
    * ADD missing codes for response filter (/search)
    
    * ADD response for error: No code file found
    
    * FIX a small compile error - removed extra brackets
    
    * UPDATE FAQ for RS IP/Port for Pune Datathon.
    
    * ADD a condition to fix a bug for code not found
    
    * UPDATE RS port ip QA for Pune Datathon'20
    
    * REMOVE irrelevant codes for subscription, media & download APIs
    
    * ADD RS API codes

--- api-examples/rs/search/java/attribute_PropertyIsGreaterThan.java
+++ api-examples/rs/search/java/attribute_PropertyIsGreaterThan.java
@@ -1,11 +1,10 @@
-OkHttpClient client = new OkHttpClient();
-
+OkHttpClient client = new OkHttpClient().newBuilder()
+		.build();
 MediaType mediaType = MediaType.parse("application/json");
-RequestBody body = RequestBody.create(mediaType, "{\n\t\"id\" : \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/pscdcl/aqm-bosch-climo/Ambedkar society circle_29\",\n\t\"attribute-name\" : \"LIGHT\",\n\t\"attribute-value\": \"2900\",\n\t\"comparison-operator\": \"propertyisgreaterthan\",\n\t\"options\":\"latest\"\n}");
+RequestBody body = RequestBody.create(mediaType, "{\n\t\"id\": \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/pscdcl/aqm-bosch-climo/Karve Statue Square_5\",\n\t\"attribute-name\" : \"LIGHT\",\n\t\"attribute-value\": \"1000\",\n\t\"comparison-operator\": \"propertyisgreaterthan\"\n}");
 Request request = new Request.Builder()
-  .url("https://pune.iudx.org.in/resource-server/pscdcl/v1/search")
-  .post(body)
-  .addHeader("Content-Type", "application/json")
-  .build();
-
+		.url("https://localhost/resource-server/pscdcl/v1/search")
+		.method("POST", body)
+		.addHeader("Content-Type", "application/json")
+		.build();
 Response response = client.newCall(request).execute();
