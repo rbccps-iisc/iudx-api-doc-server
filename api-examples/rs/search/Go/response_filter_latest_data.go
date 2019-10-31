4f9acf261665cb5c61404cfd50d6b7f2bd6b2d46
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2019-10-31 16:26:56 +0530

    ADD missing codes for response filter (/search)

---
+++ api-examples/rs/search/Go/response_filter_latest_data.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "net/http"
+  "io/ioutil"
+)
+
+func main() {
+
+  url := "https://localhost/resource-server/pscdcl/v1/search"
+  method := "POST"
+
+  payload := strings.NewReader("{\n	\"id\": \"rbccps.org/aa9d66a000d94a78895de8d4c0b3a67f3450e531/pscdcl/aqm-bosch-climo/ABC Farm House Junction_4\",\n	\"options\": \"latest\",\n	\"attribute-filter\":[\"CO_MAX\",\"SOUND\",\"STATUS\",\"LASTUPDATEDATETIME\"]\n}")
+
+  client := &http.Client {
+  }
+  req, err := http.NewRequest(method, url, payload)
+
+  if err != nil {
+    fmt.Println(err)
+  }
+  req.Header.Add("Content-Type", "application/json")
+
+  res, err := client.Do(req)
+  defer res.Body.Close()
+  body, err := ioutil.ReadAll(res.Body)
+
+  fmt.Println(string(body))
+}
